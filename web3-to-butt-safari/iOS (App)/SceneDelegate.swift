ae405f1915cbab9d43a1184ee7fbf1b78d6ee3d9
Julius Tarng <1629901+tarngerine@users.noreply.github.com>
2021-11-24 13:14:03 -0600

    new sample images, rename safari project

---
+++ web3-to-butt-safari/iOS (App)/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOS (App)
+//
+//  Created by Julius Tarng on 11/24/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
